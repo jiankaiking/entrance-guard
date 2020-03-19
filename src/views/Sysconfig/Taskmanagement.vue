60da53dadf0414df03a55037ee1b56d0f4c39023
wjk <1292934138@qq.com>
2020-03-19 16:42:13 +0800

    修改布局

--- src/views/Sysconfig/Taskmanagement.vue
+++ src/views/Sysconfig/Taskmanagement.vue
@@ -84,7 +84,7 @@
                     size:10
                 },
                 total:0,
-                listUrl:'/taskConfigManage/getTaskConfigList',
+                listUrl:'/managecenter/taskConfigManage/getTaskConfigList',
                 dialogTableVisible: false,
                 tableData: []
             }
@@ -107,23 +107,3 @@
     }
 </script>
 
-<style scoped lang="scss">
-
-
-
-
-    .tableData {
-        background: #ffffff;
-        box-shadow: 0px 1px 6px 4px rgba(242, 242, 242, 1);
-
-        .tableBox {
-            padding: 30px;
-            box-sizing: border-box;
-
-        }
-
-        .pagination {
-            padding-bottom: 30px;
-        }
-    }
-</style>
