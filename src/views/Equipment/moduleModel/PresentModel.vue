37ccb62606ccf2fb3b56ce005fa97cf46ef84d8c
wjk <1292934138@qq.com>
2020-03-19 17:09:43 +0800

    修改布局

--- src/views/Equipment/moduleModel/PresentModel.vue
+++ src/views/Equipment/moduleModel/PresentModel.vue
@@ -1,7 +1,7 @@
 <template>
     <el-form label-width="100px" :model="modelFromdata">
         <el-row>
-            <el-col :span="10">
+            <el-col :span="12">
                 <el-form-item label="代理商名称">
                     <el-input placeholder="请选择" v-model="modelFromdata.agentId" />
                 </el-form-item>
@@ -9,67 +9,67 @@
                     <el-input placeholder="请输入设备数量"  v-model="modelFromdata.deliveryCount"  />
                 </el-form-item>
             </el-col>
-            <el-col :span="10" :offset="4">
+            <el-col :span="12">
                 <el-form-item label="快递单图">
                     <uploadImg :imgUrl.sync="modelFromdata.expressOrderImg"></uploadImg>
                 </el-form-item>
             </el-col>
         </el-row>
         <el-row>
-            <el-col :span="10">
+            <el-col :span="12">
                 <el-form-item label="赠送原因">
                     <el-input placeholder="请输入内容"  v-model="modelFromdata.presentDescription"></el-input>
                 </el-form-item>
             </el-col>
-            <el-col :span="10" :offset="4">
+            <el-col :span="12">
                 <el-form-item label="设备SN码">
                     <el-input placeholder="请输入设备SN码"  v-model="modelFromdata.snCode"></el-input>
                 </el-form-item>
             </el-col>
         </el-row>
         <el-row>
-            <el-col :span="10">
+            <el-col :span="12">
                 <el-form-item label="发货厂商">
                     <el-input placeholder="请输入内容"  v-model="modelFromdata.sendOutFactoryType"></el-input>
                 </el-form-item>
             </el-col>
-            <el-col :span="10" :offset="4">
+            <el-col :span="12">
                 <el-form-item label="收货人">
                     <el-input placeholder="请输入收货人姓名"  v-model="modelFromdata.consignee"></el-input>
                 </el-form-item>
             </el-col>
         </el-row>
         <el-row>
-            <el-col :span="10">
+            <el-col :span="12">
                 <el-form-item label="厂商名称">
                     <el-input placeholder="请输入快递单号"  v-model="modelFromdata.sendOutFactoryName"></el-input>
                 </el-form-item>
             </el-col>
-            <el-col :span="10" :offset="4">
+            <el-col :span="12">
                 <el-form-item label="联系方式">
                     <el-input placeholder="请输入设备SN码" v-model="modelFromdata.consigneePhone"></el-input>
                 </el-form-item>
             </el-col>
         </el-row>
         <el-row>
-            <el-col :span="10">
+            <el-col :span="12">
                 <el-form-item label="快递单号">
                     <el-input placeholder="请输入快递单号"  v-model="modelFromdata.expressOrderNum"></el-input>
                 </el-form-item>
             </el-col>
-            <el-col :span="10" :offset="4">
+            <el-col :span="12">
                 <el-form-item label="收货地址">
                     <el-input placeholder="请输入设备SN码" v-model="modelFromdata.consigneeArea"></el-input>
                 </el-form-item>
             </el-col>
         </el-row>
         <el-row>
-            <el-col :span="10">
+            <el-col :span="12">
                 <el-form-item label="快递公司">
                     <el-input placeholder="请输入快递公司"  v-model="modelFromdata.expressCompany"></el-input>
                 </el-form-item>
             </el-col>
-            <el-col :span="10" :offset="4">
+            <el-col :span="12" >
                 <el-form-item label="详细地址">
                     <el-input placeholder="请输入详细地址"  v-model="modelFromdata.consigneeAdds"></el-input>
                 </el-form-item>
