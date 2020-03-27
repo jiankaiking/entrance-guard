38ca760061f824dd242dc41e6a00b79069f5cc22
wjk <1292934138@qq.com>
2020-03-27 15:45:37 +0800

    配置

--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
 const PREFIX = process.env.NODE_ENV === 'production' ? 'http://192.168.0.110:8888' : 'http://192.168.0.110:8888';
  //const PREFIX = process.env.NODE_ENV === 'production' ? 'http://192.168.0.166:8888' : 'hhtp://test.com';
-=======
-//const PREFIX = process.env.NODE_ENV === 'production' ? 'http://192.168.0.166:8707' : 'hhtp://test.com';
- const PREFIX = process.env.NODE_ENV === 'production' ? 'http://192.168.0.199:8888' : 'hhtp://test.com';
->>>>>>> e522541f6636df109df81127949cd13619aaa379
+
 
 export default PREFIX
 
--- vue.config.js
+++ vue.config.js
@@ -26,11 +26,8 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-<<<<<<< HEAD
                  target: 'http://192.168.0.110:8888',//managecenter sellerManagement
-=======
-                 target: 'http://192.168.0.199:8888',//managecenter sellerManagement
->>>>>>> e522541f6636df109df81127949cd13619aaa379
+
                 changeOrigin: true,
                 ws: true,
                 pathRewrite: {
