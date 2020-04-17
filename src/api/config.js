fc882fec2025ed7ae17423bf964d24f06c9f752a
wjk <1292934138@qq.com>
2020-04-17 18:32:26 +0800

    扫码登录

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 
 
-const PREFIX = process.env.NODE_ENV === 'production' ? 'http://192.168.0.108:8796' : 'https://test-gateway.lywlsz.com.cn/';
+const PREFIX = process.env.NODE_ENV === 'production' ? 'http://192.168.0.199:8888/' : 'https://test-gateway.lywlsz.com.cn/';
  //const PREFIX = process.env.NODE_ENV === 'production' ? 'http://192.168.0.166:8888' : 'hhtp://test.com';
 
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,14 +9,19 @@
                 </div>
                 <!-- 扫码登录 -->
                 <div class="sweep-login">
-                    <p>{{sweepLogin?'扫码登录':'账号密码登录'}}</p>
-
+                    <p>{{!sweepLogin?'扫码登录':'账号密码登录'}}</p>
                     <img class="checkimg" @click="checkLogin" :src="sweepLogin?'1.jpg':'2.jpg'" alt="">
-
                 </div>
                 <div class="sweep-box" v-show="sweepLogin">
-                    <p>扫一扫</p>
-                    <img src="../assets/images/logo.png" alt="">
+                    <p>请打开手机app扫一扫登录</p>
+                    <div class="code-vue">
+                        <div class="post-code-vue" v-if="codeValue == ''">
+                            <p>二维码已失效</p>
+                            <el-button @click="applyRequest">重新申请</el-button>
+                        </div>
+                        <qrcode-vue :value="codeValue" :size="200" level="H"></qrcode-vue>
+                    </div>
+
                 </div>
                 <!-- 扫码登录end -->
                 <el-form v-show="!sweepLogin" label-width="0" ref="loginForm" :model="userInfor" :rules="rules"
@@ -51,13 +56,13 @@
                         </div>
                     </div>
                 </el-form>
-
                 <div class="logingbtn">
-                    <div class="btnLogin">
+                    <div class="btnLogin" v-show="!sweepLogin">
                         <el-button class="loginBtn" @click="onSubmit('loginForm')">登录</el-button>
                     </div>
                     <p class="developerNum">开发商加版本号</p>
                 </div>
+
             </div>
         </div>
     </div>
@@ -65,14 +70,17 @@
 
 <script>
     import {mapActions, mapMutations} from 'vuex'
+    import qrcodeVue from 'qrcode-vue'
     import httpRequest from "../api/api";
     import BASE_URL from '../api/config';
+
     import axios from 'axios'
 
     export default {
         name: "Login",
         data() {
             return {
+
                 rules: {  //用户名密码验证
                     userName: [{required: true, message: '账号不可为空', trigger: 'blur'}],
                     password: [{required: true, message: '密码不可为空', trigger: 'blur'}],
@@ -85,11 +93,21 @@
                     userName: '',  //用户名
                     password: '', //密码
                     loginClient: 'PC',
-                    businessSystemID:1,
+                    businessSystemID: 1,
                     loginSncode: 'dhaskdj132132',
                     validateCode: '' //验证码
                 },
+                sweepLoginInfo: {
+                    uuid: '',
+                    loginClient: 'PC'
+                },
+                codeValue: '',
+                timeName: null,
+
             }
+        },
+        components: {
+            qrcodeVue
         },
         mounted() {
             let that = this;
@@ -100,7 +118,7 @@
                     .then(res => {
                         if (res.data.success) {
                             let userInfo = res.data.data;
-                            window.sessionStorage.setItem("user",JSON.stringify(userInfo))
+                            window.sessionStorage.setItem("user", JSON.stringify(userInfo))
                             that.auth_success({token, userInfo})
                             this.GET_ORGAN()
                             this.GET_STYEMITEM()
@@ -140,11 +158,50 @@
                 });
             },
 
-            getCode(){
-                this.codeImg = '/api/managecenter/login/getValidateCode?'+Math.random()
+            getCode() {
+                this.codeImg = '/api/managecenter/login/getValidateCode?' + Math.random()
+            },
+
+            getCheck() {
+                this.codeValue = `/managecenter/login/scanLogin?uuid=${this.sweepLoginInfo.uuid}`
+            },
+            loopRequset() {
+                httpRequest("managecenter/login/checkScanLoginPool", "POST", this.sweepLoginInfo)
+                    .then(res => {
+                        if (res.code == 200) {
+                            const token = res.data.token;
+                            const userInfo = res.data.userInfo;
+                            sessionStorage.setItem('token', token);
+                            sessionStorage.setItem('user', JSON.stringify(userInfo));
+                            this.auth_success({token, userInfo})
+                            this.$router.push('/')
+                        } else if (res.code == 300) {
+
+                        } else if (res.code == 403) {
+                            this.codeValue = '';
+                            clearTimeout(this.timeName)
+                        } else if (res.code == 401) {
+                            this.timeName = setTimeout(this.loopRequset, 1000)
+                        }
+                    })
+            },
+            //申请
+            applyRequest() {
+                httpRequest("managecenter/login/getScanLoginUuid", "get")
+                    .then(res => {
+                        if (res.success) {
+                            this.sweepLoginInfo.uuid = res.data;
+                            this.getCheck()
+                            this.loopRequset()
+                        }
+                    })
             },
             //切换登录方式
             checkLogin() {
+                clearTimeout(this.timeName)
+                if (!this.sweepLogin) {
+                    this.applyRequest()
+                }
                 this.sweepLogin = !this.sweepLogin
             },
             //忘记密码
@@ -156,6 +213,31 @@
 </script>
 
 <style lang="scss" scoped>
+
+    .code-vue {
+        padding: 15px 0;
+        position: relative;
+        width: 200px;
+        height: 200px;
+        margin: 0 auto;
+
+    }
+
+    .post-code-vue {
+        position: absolute;
+        width: 200px;
+        height: 200px;
+        z-index: 999;
+        background: rgba(0, 0, 0, .5);
+        margin-left: -50%;
+        left: 50%;
+    }
+
+    .post-code-vue p {
+        color: #fff;
+        margin: 30px 0;
+    }
+
     .el-icon-lock, .el-icon-user {
         font-size: 22px;
         margin-top: 8px;
@@ -269,6 +351,7 @@
     .login-box {
         background: #ffffff;
         box-sizing: border-box;
+        min-width: 500px;
         position: absolute;
         right: 582px;
         padding: 0px 35px 15px 35px;
